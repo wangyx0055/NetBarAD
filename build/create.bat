87e7b70f6c5ea9950338c421dc40a79884b50b69
soDLL <32153301+soDLL@users.noreply.github.com>
2018-11-14 00:04:47 +0800

    update

--- build/create.bat
+++ build/create.bat
@@ -1,2 +1,2 @@
-create.exe 6 aeskey123456 http://140.143.182.56/getClient.jsp http://140.143.182.56/click.jsp
+create.exe 9 1234567891234567 http://140.143.197.19:81/getClient.jsp?id=9 http://140.143.197.19:81/click.jsp
 pause
